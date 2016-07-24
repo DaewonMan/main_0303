ce45d6a2864f4d7a647dd6371f7be87d3516421e
Daewon Kim <eodnjs725@gmail.com>
2016-07-24 16:42:32 +0900

    Dealing with technology for Android and Java
    About intent

---
+++ Java+Android/Hello10/java/org/androidtown/hello10/MainActivity.java
@@ -0,0 +1,36 @@
+package org.androidtown.hello10;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button); // 객체 생성 및 id 찾기
+        Button button2 = (Button)findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:01010001000"));
+                startActivity(intent);
+            }
+        });
+    }
+}
